27719d54806cc5f87c84a9f894534d6531d23544
Evans <le3junkmail@gmail.com>
2019-02-28 12:55:29 -0500

    practice

---
+++ VampireGame/Assets/Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    private Vector3 movement;
+    public int speed = 0;
+    
+    private Rigidbody2D rb;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.AddForce(movement);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        //movement = transform.TransformDirection(movement);
+        //movement *= speed;
+
+    }
+}
---
+++ VampireGame/Assets/iceAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class iceAttack : MonoBehaviour {
+    public GameObject ice;
+    GameObject iceclone;
+    float IceTimer ;
+    
+	// Use this for initialization
+	void Start () {
+
+        iceclone = Instantiate(ice)as gameObject;
+	}
+    void FixedUpdate()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        IceTimer = Time.deltaTime; 
+		if(IceTimer==3)
+        {
+            Instantiate(ice);
+
+        }
+	}
+}
